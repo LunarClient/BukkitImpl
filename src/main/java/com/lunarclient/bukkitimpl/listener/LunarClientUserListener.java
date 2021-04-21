7492e1a2a1746d086a373f9f1b8db3149d6e5f99
AgentRKID <rbuhmc@gmail.com>
2021-04-21 10:19:43 -0400

    Use cached packets over waypoints (This allows less objects to be made)

--- src/main/java/com/lunarclient/bukkitimpl/listener/LunarClientUserListener.java
+++ src/main/java/com/lunarclient/bukkitimpl/listener/LunarClientUserListener.java
@@ -6,9 +6,11 @@ import com.lunarclient.bukkitapi.object.LCWaypoint;
 import com.lunarclient.bukkitapi.serverrule.LunarClientAPIServerRule;
 import com.lunarclient.bukkitimpl.LunarClient;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
 
 @RequiredArgsConstructor
 public class LunarClientUserListener implements Listener {
@@ -17,18 +19,18 @@ public class LunarClientUserListener implements Listener {
 
     @EventHandler
     public void onPlayerRegister(PlayerJoinEvent event) {
+       Player player = event.getPlayer(); 
+        
         // When the player connects, send them our disabled mods.
         if (client.getPacketModSettings() != null) {
-            LunarClientAPI.getInstance().sendPacket(event.getPlayer(), client.getPacketModSettings());
+            LunarClientAPI.getInstance().sendPacket(player, client.getPacketModSettings());
         }
 
-        LunarClientAPIServerRule.sendServerRule(event.getPlayer());
-
-        if (!client.getWaypoints().isEmpty()) {
-            for (LCWaypoint waypoint : client.getWaypoints()) {
-                LunarClientAPI.getInstance().sendWaypoint(event.getPlayer(), waypoint);
-            }
+        LunarClientAPIServerRule.sendServerRule(player);
+        
+        // Send our cached packets when they connect.
+        for (LCPacketWaypointAdd packet : client.getWaypointPackets()) {
+            LunarClientAPI.getInstance().sendPacket(player, packet);
         }
-
     }
 }
